69a36690e9ad70876f95b4040e0924663fdb2924
Annette Bowman <bowmanannette@gmail.com>
2017-03-22 18:20:49 -0400

    initial commit

---
+++ BabysitterKat/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BabysitterKat
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            BabySitterFeeCalculator babySitterFeeCalculator = new BabySitterFeeCalculator();
+            string startTime = "";
+            string endTime = "";
+            string bedTime = "";
+
+            string end = "no";
+
+            double doubleStartTime = 0.0;
+            double doubleEndTime = 0.0;
+            double doubleBedTime = 0.0;
+
+            int totalFee = 0;
+
+            while (!end.Equals("yes"))
+            {
+                Console.WriteLine("Welcome to the babysitter fee calculator!");
+                Console.WriteLine("Please enter the times in this format: XX:XXPM Midnight is 12:00AM");
+                Console.WriteLine("Please enter a start time for the babysitting service:");
+                startTime = Console.ReadLine();
+                Console.WriteLine("Please enter an end time for the babysitting service:");
+                endTime = Console.ReadLine();
+                Console.WriteLine("What time is the child's bedtime:");
+                bedTime = Console.ReadLine();
+                Console.WriteLine("DO you wish to end this:");
+                end = Console.ReadLine();
+            }
+
+        }
+        public class BabySitterFeeCalculator
+        {
+
+        }
+    }
+}
---
+++ Tests/Tests.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Tests
+{
+    public class Tests
+    {
+        BabysitterKat.Program.BabySitterFeeCalculator babySitterFeeCalculator = new BabysitterKat.Program.BabySitterFeeCalculator();
+
+        [Fact]
+        public void TakesAString()
+        {
+            string timeString = "5:00PM";
+            babySitterFeeCalculator.TakeTimeString(timeString);
+        }
+    }
+}
